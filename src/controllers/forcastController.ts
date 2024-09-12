5535a12828000963e86a9a8642fbb6e7fae657aa
Elad Moshe <eladm26@gmail.com>
2024-09-12 10:40:13 +0300

    feel_like ranked

--- src/controllers/forcastController.ts
+++ src/controllers/forcastController.ts
@@ -3,6 +3,8 @@ import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
 import { AppDataSource } from '../data-source';
 import { Forcast } from '../entity/Forcast';
+// import { Location } from '../entity/Locations';
+// import { getConnection } from 'typeorm';
 
 export const getAverageTemp = async (_1: Request, res: Response) => {
     const forcastRepository = AppDataSource.getRepository(Forcast);
@@ -34,6 +36,27 @@ export const getGlobalLowestHumidity = async (_1: Request, res: Response) => {
         .orderBy('forcast.humidity', 'ASC')
         .limit(1)
         .getRawOne();
+
+    res.status(StatusCodes.OK).json(result);
+};
+
+export const getLastFeelLikeRanked = async (req: Request, res: Response) => {
+    let orderBy = req.query?.['orderBy'] ?? 'ASC';
+    orderBy = (orderBy as string).toUpperCase()
+    console.log('orderBy', req.query?.['orderBy']);
+
+
+    const result =await AppDataSource.query(`
+        SELECT location.name, latest_forcast.feels_like
+        FROM location
+        INNER JOIN (
+          SELECT DISTINCT ON (forcast."locationId") forcast."locationId", forcast.feels_like
+          FROM forcast
+          ORDER BY forcast."locationId", forcast.ts DESC
+        ) AS latest_forcast
+        ON location.id = latest_forcast."locationId"
+        ORDER BY latest_forcast.feels_like ${orderBy}
+      `);
 
     res.status(StatusCodes.OK).json(result);
 };
--- src/routes/forcastRouter.ts
+++ src/routes/forcastRouter.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import {
     getAverageTemp,
     getGlobalLowestHumidity,
+    getLastFeelLikeRanked,
 } from '../controllers/forcastController';
 const router = Router();
 
 router.route('/average-temp').get(getAverageTemp);
 
 router.route('/lowest-humidity').get(getGlobalLowestHumidity);
+
+router.route('/last-feel-like-ranked').get(getLastFeelLikeRanked);
 
 export default router;
